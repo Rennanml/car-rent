c4b3d6b69734b0ffc556eb550742b96afddc42c1
Rennan Marcile Lazarini <rennanml112@gmail.com>
2025-09-23 17:57:26 -0300

    feat: initial authentication structure using spring security

---
+++ src/main/java/br/ifsp/vvts/DemoAuthAppApplication.java
@@ -0,0 +1,13 @@
+package br.ifsp.vvts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoAuthAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoAuthAppApplication.class, args);
+    }
+
+}
