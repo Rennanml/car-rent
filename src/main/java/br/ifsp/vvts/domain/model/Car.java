39f4421326dfddf2c293413520cda000d5fa0884
Rennan Marcile Lazarini <rennanml112@gmail.com>
2025-09-27 12:30:51 -0300

    feat: car and costumer model

---
+++ src/main/java/br/ifsp/vvts/domain/model/Car.java
@@ -0,0 +1,26 @@
+package br.ifsp.vvts.domain.model;
+
+public record Car(
+        LicensePlate licensePlate,
+        String model,
+        String color
+) {
+    public Car {
+        if (licensePlate == null) {
+            throw new IllegalArgumentException("Licence plate cannot be null");
+        }
+
+        if (model == null || model.isBlank()) {
+            throw new IllegalArgumentException("Model cannot be blank");
+        }
+
+        if (color == null || color.isBlank()) {
+            throw new IllegalArgumentException("Color cannot be null");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return model + " " + color + " - " + licensePlate.toString();
+    }
+}
---
+++ src/main/java/br/ifsp/vvts/domain/model/Costumer.java
@@ -0,0 +1,17 @@
+package br.ifsp.vvts.domain.model;
+
+public record Costumer(String name, CPF cpf) {
+    public Costumer {
+        if (name == null || name.isBlank()) {
+            throw new IllegalArgumentException("Costumer name cannot be blank");
+        }
+        if (cpf == null) {
+            throw new IllegalArgumentException("Costumer CPF cannot be null");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + cpf.format();
+    }
+}
